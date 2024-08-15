c71424c4cb750c90105432297c0bc24c8d4c6efa
gianninapaolino@outlook.com <gianninapaolino@outlook.com>
2024-08-15 10:25:49 -0300

    Todos los campos completos
    Function de alerta para avisar que hay campos incompletos

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,13 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function TodosLosCamposCompletos() {
+    if (nombre.value === "" || apellido.value === "" || email.value === "" || password1.value === "" || password2.value === "") {
+        return showAlertError(); 
+    } 
+
+let enviar = document.getElementById('regBtn');
+enviar.addEventListener("click", function() {
+    TodosLosCamposCompletos();
+});
