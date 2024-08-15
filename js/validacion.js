d34f73d54f1764df228b9e75ab6daacd151ce956
Camila Farro <camilafarroyo16842@hotmail.com>
2024-08-15 12:32:45 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -43,7 +43,7 @@ const email = document.getElementById('email').value;
 const password1 = document.getElementById('password1').value;
 const password2 = document.getElementById('password2').value;
 const terminos = document.getElementById('terminos').checked;
-   if ( checkPassword() && TodosLosCamposCompletos() && lucia()) 
+   if ( checkPassword() && TodosLosCamposCompletos() && terminosAceptados()) 
    { showAlertSuccess()} 
    else {
        showAlertError()}
