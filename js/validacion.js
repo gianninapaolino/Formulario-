601f2243f5b737a1d1f01be690e1f6ff120dd269
Camila Farro <camilafarroyo16842@hotmail.com>
2024-08-15 11:22:40 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,10 @@
+const nombre = document.getElementById('nombre').value;
+const apellido = document.getElementById('apellido').value;
+const email = document.getElementById('email').value;
+const password1 = document.getElementById('password1').value;
+const password2 = document.getElementById('password2').value;
+const terminos = document.getElementById('terminos').checked;
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
@@ -20,13 +27,6 @@ enviar.addEventListener("click", function() {
        });
 
 }
-
-const nombre = document.getElementById('nombre').value;
-const apellido = document.getElementById('apellido').value;
-const email = document.getElementById('email').value;
-const password1 = document.getElementById('password1').value;
-const password2 = document.getElementById('password2').value;
-const terminos = document.getElementById('terminos').checked;
 
 
 let enviar = document.getElementById("regBtn");
