18bd8c2711cff085c8095d2585b53658c4a6764b
erikamaidana1950 <erikamaidana50@gmail.com>
2024-08-15 09:27:20 -0300

    funcion contraseña

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,16 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+let passw1 = document.getElementById("password1")
+let passw2 = document.getElementById("password2")
+
+function mensajeAceptar() {
+
+if (passw1 === passw2) { 
+   console.log (showAlertSuccess());
+}
+  else  {
+
+     console.log (showAlertError());
+ }
+}
